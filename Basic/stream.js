4777f28b79e1d2c11dbcda031222097b3d3cd5f9
Louis Phan <sutrix.tu.phan@sutrixsolutions.com>
2017-02-21 15:13:44 +0700

    Init

---
+++ Basic/stream.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var data = '';
+
+//Create readable stream
+var readerStream = fs.createReadStream('/media/DATA/Node/Basic/input.txt');
+
+//Set encoding UTF-8
+readerStream.setEncoding('UTF-8');
+
+// Handle stream events --> data, end, and error
+readerStream.on('data', function(chunk) {
+   data += chunk;
+});
+
+readerStream.on('end',function(){
+   console.log(data);
+});
+
+readerStream.on('error', function(err){
+   console.log(err.stack);
+});
+
+console.log("Program Ended");
+
+var fs = require("fs");
+var data = 'Simply Easy Learning';
+
+// Create a writable stream
+var writerStream = fs.createWriteStream('output.txt');
+
+// Write the data to stream with encoding to be utf8
+writerStream.write(data,'UTF8');
+
+// Mark the end of file
+writerStream.end();
+
+// Handle stream events --> finish, and error
+writerStream.on('finish', function() {
+    console.log("Write completed.");
+});
+
+writerStream.on('error', function(err){
+   console.log(err.stack);
+});
+
+console.log("Program Ended");
